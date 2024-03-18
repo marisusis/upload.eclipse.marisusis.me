2bdfbdf2e9a89b092fde3ada4f016f104912a6ca
Maris <mau13@case.edu>
2024-03-18 00:02:40 -0400

    Update

--- app/src/App.js
+++ app/src/App.js
@@ -1,76 +1,252 @@
 import "./App.scss";
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useReducer, useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import UploadItem from "./components/UploadItem";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import UploadItemList from "./components/UploadItemList";
+
+function itemsReducer(items, action) {
+  switch (action.type) {
+    case "add": {
+      return [
+        ...items,
+        {
+          id: items.length > 0 ? items[items.length - 1].id + 1 : 0,
+          file: null,
+          progress: {
+            loaded: 0,
+            total: 0,
+          },
+          state: "idle",
+        },
+      ];
+    }
+    case "remove": {
+      return items.filter((item) => item.id !== action.id);
+    }
+    case "update": {
+      return items.map((item) => {
+        if (item.id === action.item.id) {
+          return action.item;
+        }
+        return item;
+      });
+    }
+    default: {
+      console.error("Unknown action", action);
+      return items;
+    }
+  }
+}
 
 function App() {
   let callsignInputRef = useRef(null);
-  let uploadButtonRef = useRef(null);
-  let addFilesButtonRef = useRef(null);
-  let uploads_ref = useRef(null);
+  let passwordInputRef = useRef(null);
+  let [uploading, setUploading] = useState(false);
 
-  useEffect(() => {
-    let button = uploadButtonRef.current;
-    const callsignInput = callsignInputRef.current;
+  const uploadItems = async (items) => {
+    for (let item of items) {
+      try {
+        item = {
+          ...item,
+          state: "uploading",
+        };
 
-    let onClick = () => {
-      console.log(`Uploading files from ${callsignInput.value}...`);
-    };
+        dispatch({
+          type: "update",
+          item: item,
+        });
 
-    button.addEventListener("click", onClick);
+        let response = await fetch(
+          "http://localhost:8080/api/upload?" +
+            new URLSearchParams({
+              key: passwordInputRef.current.value,
+              user: callsignInputRef.current.value,
+              file: item.file.name,
+            }),
+        );
 
-    return () => {
-      button.removeEventListener("click", onClick);
-    };
-  }, [uploadButtonRef, callsignInputRef]);
+        if (response.status === 401) {
+          throw new Error("Unauthorized");
+        } else if (response.status !== 200) {
+          throw new Error("Internal error");
+        }
 
-  useEffect(() => {
-    const button = addFilesButtonRef.current;
+        let json = await response.json();
+        let xhr = new XMLHttpRequest();
+        xhr.open("PUT", json.url, true);
 
-    const onClick = () => {
-      console.debug("Adding file upload field...");
-    };
+        xhr.upload.onprogress = function (e) {
+          if (e.lengthComputable) {
+            dispatch({
+              type: "update",
+              item: {
+                ...item,
+                progress: {
+                  loaded: e.loaded,
+                  total: e.total,
+                },
+              },
+            });
+          }
+        };
 
-    button.addEventListener("click", onClick);
+        xhr.send(item.file);
 
-    return () => {
-      button.removeEventListener("click", onClick);
-    };
-  }, [addFilesButtonRef]);
+        await new Promise((resolve, reject) => {
+          console.log("Awaiting upload");
+          xhr.onload = resolve;
+          xhr.onerror = reject;
+        });
 
-  let onClose = (id) => {
-    console.debug(`Creating close handler for file upload ${id}`);
-    return () => {
-      console.log(`Closing ${id}`);
-    };
+        dispatch({
+          type: "update",
+          item: {
+            ...item,
+            state: "uploaded",
+          },
+        });
+
+        setTimeout(() => {
+          dispatch({
+            type: "remove",
+            id: item.id,
+          });
+        }, 5000);
+      } catch (e) {
+        console.error("Error uploading", e);
+        dispatch({
+          type: "update",
+          item: {
+            ...item,
+            state: "error",
+            error: e,
+          },
+        });
+      }
+    }
   };
 
-  let fields = [
-    <UploadItem onClose={onClose(1)} />,
-    <UploadItem onClose={onClose(1)} />,
-    <UploadItem onClose={onClose(1)} />,
-    <UploadItem onClose={onClose(1)} />,
-    <UploadItem onClose={onClose(1)} />,
-  ];
+  const [items, dispatch] = useReducer(itemsReducer, [
+    {
+      state: "idle",
+      id: 0,
+      file: null,
+      progress: {},
+    },
+  ]);
+
+  const onUpload = () => {
+    if (uploading) {
+      return;
+    }
+
+    let populated_items = items.filter(
+      (item) => item.file !== null && item.state !== "uploaded",
+    );
+
+    populated_items.forEach((item) => {
+      dispatch({
+        type: "update",
+        item: {
+          ...item,
+          state: "queued",
+        },
+      });
+    });
+
+    uploadItems(populated_items).then(() => {
+      setUploading(false);
+    });
+
+    setUploading(true);
+  };
+
+  const handleAddItem = () => {
+    dispatch({ type: "add" });
+  };
+
+  const handleRemoveItem = (id) => {
+    dispatch({ type: "remove", id: id });
+  };
+  const handleUpdateItem = (item) => {
+    dispatch({ type: "update", item: item });
+  };
+
+  let uploadContent = <></>;
+
+  if (uploading) {
+    uploadContent = (
+      <>
+        <p>Uploading...</p>
+        {/* <ProgressBar
+          striped
+          variant="primary"
+          animated
+          now={uploadProgress}
+          style={{
+            height: "40px",
+          }}
+          label={`Uploading...`}
+        ></ProgressBar> */}
+      </>
+    );
+  } else {
+    uploadContent = (
+      <div class="d-flex justify-content-end formControl mb-1">
+        <button
+          class="btn center btn-lg btn-primary"
+          style={{ width: "100px" }}
+          onClick={onUpload}
+        >
+          Upload
+        </button>
+      </div>
+    );
+  }
 
   return (
-    <Container>
-      <div className="card">
-        <h1 className="card-header">Eclipse File Upload</h1>
+    <Container
+      style={{
+        maxWidth: "800px",
+      }}
+    >
+      <h1
+        className="card-header mt-5"
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Eclipse File Upload
+      </h1>
+      <div className="card mt-5">
         <div class="card-body">
           <h5 class="card-title">Welcome to the eclipse file uploader!</h5>
           <p class="card-text">
             First enter your callsign, then select the files you want to upload.
-            To add additional files, click the "Add file" button. When you're
-            ready, click the "Upload" button to upload your files. If you added
-            a field by mistake, don't worry. If no file is selected, the field
-            will be ignored.
+            To add additional files, click the{" "}
+            <span class="text-secondary">"Add file"</span> button. When you're
+            ready, click the <span class="text-primary">"Upload"</span> button
+            to upload your files. If you added a field by mistake, don't worry.
+            If no file is selected, the field will be ignored.
+            <b> Please make sure to name your files in a descriptive manner.</b>
           </p>
-          <div ref={uploads_ref} className="gx-5 container mb-3">
+          <div className="gx-5 container mb-3">
             <div className="card p-3">
               <div className="mb-3">
                 <label for="exampleFormControlInput1" class="form-label">
-                  Callsign
+                  Password
+                </label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleFormControlInput1"
+                  ref={passwordInputRef}
+                />
+              </div>
+              <div className="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">
+                  Callsign or Name
                 </label>
                 <input
                   type="email"
@@ -81,27 +257,23 @@ function App() {
                 />
               </div>
               <div class="mb-3">
-                {fields}
-                <div class="mb-3">
-                  <button
-                    class="btn btn-sm btn-light formControl"
-                    ref={addFilesButtonRef}
-                  >
-                    Add file
-                  </button>
-                </div>
+                <UploadItemList
+                  items={items}
+                  onRemoveItem={handleRemoveItem}
+                  onUpdateItem={handleUpdateItem}
+                  onAddItem={handleAddItem}
+                />
               </div>
-              <div class="d-flex justify-content-end formControl">
-                <button
-                  class="btn center btn-lg btn-primary"
-                  style={{ width: "100px" }}
-                  ref={uploadButtonRef}
-                >
-                  Upload
-                </button>
-              </div>
+
+              {uploadContent}
             </div>
           </div>
+          <p class="fs-6">
+            <em>
+              Made by <a href="https://github.com/marisusis">@marisusis</a> for
+              the total solar eclipse on April 8, 2024.
+            </em>
+          </p>
         </div>
       </div>
     </Container>
--- app/src/components/UploadItem.js
+++ app/src/components/UploadItem.js
@@ -1,60 +1,103 @@
 import React, { useRef, useEffect, useState } from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
 
-export default function UploadItem({ onClose }) {
+export default function UploadItem({ item, onRemove, onUpdate }) {
   const input_ref = useRef(null);
-  const button_ref = useRef(null);
-
-  useEffect(() => {
-    const inputElement = input_ref.current;
-
-    let onChange = (event) => {
-      console.log(event);
-    };
-
-    inputElement.addEventListener("change", onChange);
-
-    return () => {
-      inputElement.removeEventListener("change", onChange);
-    };
-  }, [input_ref]);
-
-  useEffect(() => {
-    const buttonElement = button_ref.current;
-
-    let onClick = (event) => {
-      onClose();
-    };
-
-    buttonElement.addEventListener("click", onClick);
-
-    return () => {
-      buttonElement.removeEventListener("click", onClick);
-    };
-  }, [button_ref, onClose]);
-
-  return (
-    <div class="row gy-2 gx-3 mb-3">
-      <div className="col col-auto">
+  let content = null;
+  if (item.state == "idle") {
+    content = (
+      <>
+        <div className="col col-md-10 col-sm-9 col-xs-12">
+          <input
+            className="form-control form-control"
+            ref={input_ref}
+            type="file"
+            id="formFileMultiple"
+            style={{
+              width: "100%",
+            }}
+            onChange={(e) => {
+              onUpdate({
+                ...item,
+                file: e.target.files[0],
+              });
+            }}
+          />
+        </div>
+        <div className="col col-md-2 col-sm-3 col-xs-1">
+          <button
+            onClick={() => onRemove(item.id)}
+            type="button"
+            className="btn btn-link"
+          >
+            Remove
+          </button>
+        </div>
+      </>
+    );
+  } else if (item.state === "uploading") {
+    content = (
+      <div className="col col-md-12 col-sm-12 col-xs-12">
+        <ProgressBar
+          striped
+          variant="secondary"
+          animated
+          now={(item.progress.loaded / item.progress.total) * 100}
+          style={{
+            height: "40px",
+          }}
+          label={item.progress.loaded + " / " + item.progress.total}
+        ></ProgressBar>
+      </div>
+    );
+  } else if (item.state === "uploaded") {
+    content = (
+      <>
+        <div className="col col-md-12 col-sm-12 col-xs-12">
+          <div class="text-bg-light border-success text-success rounded border p-2">
+            Uploaded {item.file.name.substr(0, 30)}
+            {item.file.name.length > 30 ? "..." : ""}
+          </div>
+        </div>
+      </>
+    );
+  } else if (item.state === "error") {
+    content = (
+      <>
+        <div className="col col-md-10 col-sm-9 col-xs-12">
+          <div className="text-bg-light border-danger text-danger rounded border p-2">
+            Error uploading {item.file.name.substr(0, 20)}...:
+            {item.error.toString()}
+          </div>
+        </div>
+        <div className="col col-md-2 col-sm-3 col-xs-1">
+          <button
+            onClick={() => onRemove(item.id)}
+            type="button"
+            className="btn btn-link"
+          >
+            Remove
+          </button>
+        </div>
+      </>
+    );
+  } else if (item.state === "queued") {
+    content = (
+      <div className="col col-md-12 col-sm-12 col-xs-12">
         <input
           className="form-control form-control"
           ref={input_ref}
-          type="file"
+          type="text"
           id="formFileMultiple"
           style={{
-            minWidth: "400px",
             width: "100%",
           }}
+          disabled
+          placeholder={`Queued: ${item.file.name}`}
         />
       </div>
-      <div className="col col-auto">
-        <button
-          ref={button_ref}
-          type="button"
-          className="btn btn-outline-danger"
-        >
-          Remove
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
+
+  return <div class="row gy-2 gx-3 mb-3">{content}</div>;
 }
